03c162dfea6c5b15874f3fbc340df5317a4147a0
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2021-07-18 11:27:15 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 const app = new Application();
 
 app.use((ctx) => {
-  ctx.response= {data:"success login"}
+  ctx.response.body = {data:"success login"}
 });
 
 addEventListener("fetch",app.fetchEventHandler());
